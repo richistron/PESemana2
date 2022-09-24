a67af6c24582e18a17fa3b84c9c3c2ac53a9321b
Ricardo Rivas <richistron@gmail.com>
2022-09-23 19:36:58 -0700

    Agrega Análisis de Caso Semana 2

---
+++ AnalsisDeCasoS2/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Se necesita desarrollar un programa que solicite al usuario 3 cantidades, pueden ser enteros o décimales, 
+// posteriormente  realice la suma de éstos y  muestre en resultado obtenido en pantalla. 
+
+namespace AnalisisDeCaso
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] numeros = SolicitaNumeros();
+            Console.WriteLine($"La suma de los números es: {numeros.Sum().ToString()}");
+        }
+
+        private static int[] SolicitaNumeros()
+        {
+            int[] numeros = new int[3];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = LeeNumero();
+            }
+
+            return numeros;
+        }
+
+        private static int LeeNumero()
+        {
+            int numero = 0;
+            string entrada;
+            bool numeroValido = false;
+
+            while (!numeroValido)
+            {
+                Console.WriteLine("Ingresa un número");
+                entrada = Console.ReadLine();
+                if (!int.TryParse(entrada, out numero))
+                    Console.WriteLine("El Número es invalido");
+                else numeroValido = true;
+            }
+
+            return numero;
+        }
+    }
+}
